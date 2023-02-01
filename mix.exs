6f5be73e009d90ccfe93a7040792310e426cb25f
Francisco Marques <franciscopcmarques@gmail.com>
2023-02-01 00:27:34 +0000

    added ex_doc and mix updates

--- mix.exs
+++ mix.exs
@@ -4,25 +4,24 @@ defmodule AbsintheUtils.MixProject do
   def project do
     [
       app: :absinthe_utils,
-      version: "0.1.0",
+      version: System.get_env("APP_VERSION", "0.0.0-development"),
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:absinthe, "~> 1.7"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
 end

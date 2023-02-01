7958d49abc516ba06f1904e7381d69825c75745f
Francisco Marques <franciscopcmarques@gmail.com>
2023-02-01 12:24:34 +0000

    added null tests to deprecated args

--- test/middleware/deprecated_args_test.exs
+++ test/middleware/deprecated_args_test.exs
@@ -64,6 +64,25 @@ defmodule AbsintheUtilsTest.Middleware.DeprecatedArgsTest do
           {:ok, Map.get(params, :new_arg)}
         end)
       end
+
+      field :query_with_deprecated_optional_non_null_args, :string do
+        arg(:old_arg, :string, deprecate: "Use `newParam` instead.")
+        arg(:new_arg, :string)
+
+        middleware(
+          DeprecatedArgs,
+          %{
+            legacy_arg_identifier: :old_arg,
+            new_arg_identifier: :new_arg,
+            is_required: false,
+            non_null: true
+          }
+        )
+
+        resolve(fn _, params, _ ->
+          {:ok, Map.get(params, :new_arg)}
+        end)
+      end
     end
   end
 
@@ -195,6 +214,33 @@ defmodule AbsintheUtilsTest.Middleware.DeprecatedArgsTest do
                  TestSchema,
                  variables: %{
                    "newArg" => "sample value"
+                 }
+               )
+    end
+
+    test "passing new argument as null" do
+      assert {:ok,
+              %{
+                errors: [
+                  %{
+                    extensions: %{code: "NOT_NULL_VIOLATION"},
+                    message: "Argument newArg cannot be null"
+                  }
+                ]
+              }} =
+               Absinthe.run(
+                 """
+                   query (
+                     $newArg: String
+                   ) {
+                     queryWithDeprecatedRequiredNonNullArgs(
+                      newArg: $newArg
+                     )
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "newArg" => nil
                  }
                )
     end
@@ -374,6 +420,160 @@ defmodule AbsintheUtilsTest.Middleware.DeprecatedArgsTest do
                  """
                    query {
                      queryWithDeprecatedOptionalArgs
+                   }
+                 """,
+                 TestSchema
+               )
+    end
+  end
+
+  describe "optional non null argument" do
+    test "passing new argument" do
+      assert {:ok,
+              %{
+                data: %{
+                  "queryWithDeprecatedOptionalNonNullArgs" => "sample value"
+                }
+              }} ===
+               Absinthe.run(
+                 """
+                   query (
+                     $newArg: String
+                   ) {
+                     queryWithDeprecatedOptionalNonNullArgs(
+                      newArg: $newArg
+                     )
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "newArg" => "sample value"
+                 }
+               )
+    end
+
+    test "passing old argument" do
+      assert {:ok,
+              %{
+                data: %{
+                  "queryWithDeprecatedOptionalNonNullArgs" => "sample value"
+                }
+              }} ===
+               Absinthe.run(
+                 """
+                   query (
+                     $oldArg: String
+                   ) {
+                     queryWithDeprecatedOptionalNonNullArgs(
+                      oldArg: $oldArg
+                     )
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "oldArg" => "sample value"
+                 }
+               )
+    end
+
+    test "passing new argument as null" do
+      assert {:ok,
+              %{
+                errors: [
+                  %{
+                    extensions: %{code: "NOT_NULL_VIOLATION"},
+                    message: "Argument newArg cannot be null"
+                  }
+                ]
+              }} =
+               Absinthe.run(
+                 """
+                   query (
+                     $newArg: String
+                   ) {
+                     queryWithDeprecatedOptionalNonNullArgs(
+                      newArg: $newArg
+                     )
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "newArg" => nil
+                 }
+               )
+    end
+
+    test "passing old argument as null" do
+      assert {:ok,
+              %{
+                errors: [
+                  %{
+                    extensions: %{code: "NOT_NULL_VIOLATION"},
+                    message: "Argument oldArg cannot be null"
+                  }
+                ]
+              }} =
+               Absinthe.run(
+                 """
+                   query (
+                     $oldArg: String
+                   ) {
+                     queryWithDeprecatedOptionalNonNullArgs(
+                      oldArg: $oldArg
+                     )
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "oldArg" => nil
+                 }
+               )
+    end
+
+    test "passing both legacy and new arguments" do
+      assert {
+               :ok,
+               %{
+                 data: %{"queryWithDeprecatedOptionalNonNullArgs" => nil},
+                 errors: [
+                   %{
+                     extensions: %{code: "MUTUALLY_EXCLUSIVE_ARG_VIOLATION"},
+                     message: "Arguments oldArg and newArg cannot be passed together"
+                   }
+                 ]
+               }
+             } =
+               Absinthe.run(
+                 """
+                   query (
+                     $newArg: String
+                     $oldArg: String
+                   ) {
+                     queryWithDeprecatedOptionalNonNullArgs(
+                      newArg: $newArg
+                      oldArg: $oldArg
+                     )
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "newArg" => "ignored",
+                   "oldArg" => "ignored"
+                 }
+               )
+    end
+
+    test "not passing any argument" do
+      assert {
+               :ok,
+               %{
+                 data: %{"queryWithDeprecatedOptionalNonNullArgs" => nil}
+               }
+             } ===
+               Absinthe.run(
+                 """
+                   query {
+                     queryWithDeprecatedOptionalNonNullArgs
                    }
                  """,
                  TestSchema

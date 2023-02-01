9f6313cea35969065b2ced62c9b37e04199402cb
Francisco Marques <franciscopcmarques@gmail.com>
2023-02-01 00:27:51 +0000

    added more loader tests

--- test/middleware/arg_loader_test.exs
+++ test/middleware/arg_loader_test.exs
@@ -3,31 +3,40 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
 
   alias AbsintheUtils.Middleware.ArgLoader
 
-  defmodule SampleUserApi do
+  defmodule SampleRepository do
+    @moduledoc """
+    This is a demo api used as a "mock" called by resolvers.
+    This test case would probably be better tested with real mocks on the resolvers
+     so we could assert calls and have more control of the flow.
+     But for the ease of development this was chosen for now.
+    """
     @users [
       %{id: "1", name: "Ally"},
       %{id: "2", name: "Bob"}
     ]
 
-    def get_user(id) do
+    def get_by_id(enumerable, id) do
       {
         :ok,
         Enum.find(
-          @users,
-          fn user -> user.id == id end
+          enumerable,
+          fn item -> item.id == id end
         )
       }
     end
 
-    def get_users(ids) do
+    def filter_by_ids(enumerable, ids) do
       {
         :ok,
         Enum.filter(
-          @users,
-          fn user -> user.id in ids end
+          enumerable,
+          fn item -> item.id in ids end
         )
       }
     end
+
+    def get_user(id), do: get_by_id(@users, id)
+    def get_users(ids), do: filter_by_ids(@users, ids)
   end
 
   defmodule TestSchema do
@@ -47,7 +56,7 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
           %{
             id: [
               new_name: :user,
-              load_function: &SampleUserApi.get_user/1
+              load_function: &SampleRepository.get_user/1
             ]
           }
         )
@@ -65,7 +74,7 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
           %{
             ids: [
               new_name: :users,
-              load_function: &SampleUserApi.get_users/1
+              load_function: &SampleRepository.get_users/1
             ]
           }
         )
@@ -74,11 +83,40 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
           {:ok, Map.get(params, :users)}
         end)
       end
+
+      field :two_users, non_null(list_of(:user)) do
+        arg(:user1_id, :id)
+        arg(:user2_id, :id)
+
+        middleware(
+          ArgLoader,
+          %{
+            user1_id: [
+              new_name: :user1,
+              load_function: &SampleRepository.get_user/1
+            ],
+            user2_id: [
+              new_name: :user2,
+              load_function: &SampleRepository.get_user/1
+            ]
+          }
+        )
+
+        resolve(fn _, params, _ ->
+          {
+            :ok,
+            [
+              Map.get(params, :user1),
+              Map.get(params, :user2)
+            ]
+          }
+        end)
+      end
     end
   end
 
-  describe "argument" do
-    test "loading user by id" do
+  describe "loading one argument" do
+    test "success" do
       assert {:ok,
               %{
                 data: %{
@@ -108,7 +146,7 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
                )
     end
 
-    test "argument not passed" do
+    test "optional not passed" do
       assert {:ok,
               %{
                 data: %{
@@ -128,7 +166,7 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
                )
     end
 
-    test "user not found" do
+    test "not found" do
       assert {:ok,
               %{
                 errors: [
@@ -160,8 +198,8 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
     end
   end
 
-  describe "list argument" do
-    test "loading users by ids" do
+  describe "loading one array argument" do
+    test "success" do
       assert {:ok,
               %{
                 data: %{
@@ -247,6 +285,134 @@ defmodule AbsintheUtilsTest.Middleware.ArgLoaderTest do
                  TestSchema,
                  variables: %{
                    "ids" => ["1", "invalid", "2"]
+                 }
+               )
+    end
+  end
+
+  describe "loading two arguments" do
+    test "success" do
+      assert {
+               :ok,
+               %{
+                 data: %{
+                   "twoUsers" => [
+                     %{"id" => "2", "name" => "Bob"},
+                     %{"id" => "1", "name" => "Ally"}
+                   ]
+                 }
+               }
+             } ===
+               Absinthe.run(
+                 """
+                   query (
+                     $user1Id: ID!
+                     $user2Id: ID!
+                   ) {
+                    twoUsers (
+                      user1Id: $user1Id
+                      user2Id: $user2Id
+                     ) {
+                      id
+                      name
+                     }
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "user1Id" => "2",
+                   "user2Id" => "1"
+                 }
+               )
+    end
+
+    test "one not provided" do
+      assert {
+               :ok,
+               %{
+                 data: %{
+                   "twoUsers" => [
+                     nil,
+                     %{"id" => "2", "name" => "Bob"}
+                   ]
+                 }
+               }
+             } ===
+               Absinthe.run(
+                 """
+                   query (
+                     $user2Id: ID!
+                   ) {
+                    twoUsers (
+                      user2Id: $user2Id
+                     ) {
+                      id
+                      name
+                     }
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "user2Id" => "2"
+                 }
+               )
+    end
+
+    test "none provided" do
+      assert {
+               :ok,
+               %{
+                 data: %{
+                   "twoUsers" => [nil, nil]
+                 }
+               }
+             } ===
+               Absinthe.run(
+                 """
+                   query {
+                    twoUsers {
+                      id
+                      name
+                     }
+                   }
+                 """,
+                 TestSchema
+               )
+    end
+
+    test "error one not found" do
+      assert {
+               :ok,
+               %{
+                 data: nil,
+                 errors: [
+                   %{
+                     extensions: %{code: "NOT_FOUND"},
+                     message:
+                       "The entity(ies) provided in the following arg(s), could not be found: user2_id"
+                   }
+                 ]
+               }
+             } =
+               Absinthe.run(
+                 """
+                   query (
+                     $user1Id: ID!
+                     $user2Id: ID!
+                   ) {
+                    twoUsers (
+                      user1Id: $user1Id
+                      user2Id: $user2Id
+                     ) {
+                      id
+                      name
+                     }
+                   }
+                 """,
+                 TestSchema,
+                 variables: %{
+                   "user1Id" => "1",
+                   "user2Id" => "invalid"
                  }
                )
     end

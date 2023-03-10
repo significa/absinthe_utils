7d85a8f872e52566d53210f45d2b79a597716840
Francisco Marques <franciscopcmarques@gmail.com>
2023-03-10 18:59:24 +0000

    feat: sorting utils cleaning

--- lib/helpers/sort.ex
+++ lib/helpers/sort.ex
@@ -1,4 +1,4 @@
-defmodule AbsintheUtils.Helpers.Sorted do
+defmodule AbsintheUtils.Helpers.Sort do
   def sort_alike(
         unsorted_enumerable,
         sorted_enumerable,
@@ -7,7 +7,8 @@ defmodule AbsintheUtils.Helpers.Sorted do
       ) do
     value_to_index =
       sorted_enumerable
-      |> _map_element_to_index(sorted_enumerable_mapper)
+      |> Enum.map(sorted_enumerable_mapper)
+      |> map_enum_to_index()
 
     unsorted_enumerable
     |> Enum.sort_by(fn element ->
@@ -18,9 +19,18 @@ defmodule AbsintheUtils.Helpers.Sorted do
     end)
   end
 
-  def _map_element_to_index(enumerable, mapper \\ & &1) do
+  @doc """
+  Given an enumerable, returns a map of each element
+  to the position in the original enumerable (zero based).
+
+  ## Examples
+
+      iex> Sort.map_enum_to_index([:a, :b, :c])
+      %{a: 0, b: 1, c: 2}
+  """
+  @spec map_enum_to_index(list(any)) :: map
+  def map_enum_to_index(enumerable) do
     enumerable
-    |> Enum.map(mapper)
     |> Enum.with_index()
     |> Map.new(fn {element, index} -> {element, index} end)
   end
--- test/helpers/sort_test.exs
+++ test/helpers/sort_test.exs
@@ -1,29 +1,31 @@
 defmodule AbsintheUtils.Helpers.SortTest do
   use ExUnit.Case, async: true
 
-  alias AbsintheUtils.Helpers.Sorted
+  alias AbsintheUtils.Helpers.Sort
+
+  doctest AbsintheUtils.Helpers.Sort
 
   describe "sort_alike" do
     test "empty lists" do
-      assert [] === Sorted.sort_alike([], [])
+      assert [] === Sort.sort_alike([], [])
     end
 
     test "unsorted empty" do
-      assert [] === Sorted.sort_alike([], [1, 2, 3])
+      assert [] === Sort.sort_alike([], [1, 2, 3])
     end
 
     test "unsorted contains unknown elements" do
       assert_raise KeyError, fn ->
-        Sorted.sort_alike([8], [1, 2, 3])
+        Sort.sort_alike([8], [1, 2, 3])
       end
     end
 
     test "sorted contains more elements" do
-      assert [1] === Sorted.sort_alike([1], [1, 2, 3])
+      assert [1] === Sort.sort_alike([1], [1, 2, 3])
     end
 
     test "all elements match" do
-      assert [2, 3, 1] === Sorted.sort_alike([1, 2, 3], [2, 3, 1])
+      assert [2, 3, 1] === Sort.sort_alike([1, 2, 3], [2, 3, 1])
     end
 
     test "with unsorted mapper" do
@@ -32,7 +34,7 @@ defmodule AbsintheUtils.Helpers.SortTest do
                %{id: 1},
                %{id: 2}
              ] ===
-               Sorted.sort_alike(
+               Sort.sort_alike(
                  [
                    %{id: 1},
                    %{id: 2},
@@ -49,7 +51,7 @@ defmodule AbsintheUtils.Helpers.SortTest do
                %{id: 3},
                %{id: 1}
              ] ===
-               Sorted.sort_alike(
+               Sort.sort_alike(
                  [
                    %{id: 1},
                    %{id: 2},
@@ -66,9 +68,9 @@ defmodule AbsintheUtils.Helpers.SortTest do
     end
   end
 
-  describe "_map_element_to_index" do
+  describe "map_enum_to_index" do
     test "empty list" do
-      assert %{} === Sorted._map_element_to_index([])
+      assert %{} === Sort.map_enum_to_index([])
     end
 
     test "no mapper" do
@@ -76,23 +78,7 @@ defmodule AbsintheUtils.Helpers.SortTest do
                1 => 0,
                2 => 1,
                3 => 2
-             } === Sorted._map_element_to_index([1, 2, 3])
-    end
-
-    test "with mapper" do
-      assert %{
-               "John" => 0,
-               "Ally" => 1,
-               "Sam" => 2
-             } ===
-               Sorted._map_element_to_index(
-                 [
-                   %{name: "John"},
-                   %{name: "Ally"},
-                   %{name: "Sam"}
-                 ],
-                 & &1.name
-               )
+             } === Sort.map_enum_to_index([1, 2, 3])
     end
   end
 end

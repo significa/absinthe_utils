dec754645a1f8878047fdb9e3a1232cb480035b3
Francisco Marques <tofran@users.noreply.github.com>
2025-05-05 14:44:04 +0100

    implement deep getter and setter on arg loader

---
+++ test/interna/map_helpers_test.exs
@@ -0,0 +1,9 @@
+defmodule AbsintheUtils.Internal.MapHelpersTest do
+  @moduledoc false
+
+  use ExUnit.Case
+
+  alias AbsintheUtils.Internal.MapHelpers
+
+  doctest AbsintheUtils.Internal.MapHelpers
+end
